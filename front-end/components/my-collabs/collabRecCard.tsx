9a7def6fc8c55150426f47c4afae922af6011a40
Shakiran <8shak.s@gmail.com>
2020-07-10 16:00:01 -0400

    Restyled collab req recieve card

--- front-end/components/my-collabs/collabRecCard.tsx
+++ front-end/components/my-collabs/collabRecCard.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import collabCompStyles from "./collabComps.module.scss";
 import Link from 'next/link';
+import moment from 'moment';
 
 interface Props {
     title:string,
     description:string,
-    date?:number,
+    date:number,
     collaborator:{userId: string, username: string}
     changeCollabRecStatus:(collabReq:{username:string, accept:boolean}) => void
 }
 export default (props: Props) => {
+    let dateObj = moment( new Date(props.date)).format('YYYY-MM-DD')
     const onAcceptRequest = () =>{
         props.changeCollabRecStatus({username:props.collaborator.username, accept:true});
     }
@@ -20,9 +22,9 @@ export default (props: Props) => {
         <div className={collabCompStyles.collabRecCard}>
             <div className={collabCompStyles.flexCardHeader}>
                 <h3>{props.title}</h3>
-                <Link href={`/profiles/${props.collaborator.username}`}><a className={collabCompStyles.profileLink}>Collab with {props.collaborator.userId}</a></Link> 
+                <Link href={`/profiles/${props.collaborator.username}`}><a className={collabCompStyles.profileLink}>View {props.collaborator.username}'s profile</a></Link> 
             </div>
-            <span>{props.date}</span>
+            <span>{dateObj}</span>
             <p className={collabCompStyles.description}>{props.description}</p>
             <button onClick={onAcceptRequest} className={collabCompStyles.acceptButton}>Accept Request</button><button onClick={onDenyRequest} className={collabCompStyles.denyButton}>Deny Request</button>
         </div>
