128e3b6564ffb0fd432e04740f3453856803dbef
Shakiran Sathiyanathan <40652815+8shaks@users.noreply.github.com>
2023-10-10 01:29:44 -0400

    Update keys_dev.ts

--- back-end/src/config/keys_dev.ts
+++ back-end/src/config/keys_dev.ts
@@ -1,11 +1,5 @@
 const keys_dev = {
-  mongoURI: "mongodb+srv://shakiran_s:8sj3CSeYV6@kloud-ulen6.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: 'TSCGHT',
-  resetPassword:"ASD*@NCVSAHJ)A@",
-  accessKeyId:"AKIAJH52AAB5XEIEZQXQ",
-  secretAccessKey: "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",
-  email:"officialkloudmail@gmail.com",
-  emailPassword:"dugfiq-kynfat-0gomRi"
+
 }
 
 module.exports =  keys_dev
